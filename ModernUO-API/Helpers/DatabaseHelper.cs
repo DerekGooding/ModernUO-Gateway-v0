f4994a5264fa5ff7dbcd8921db5cd8ce509aabd9
Derek Gooding <djgooding@live.com>
2024-09-19 14:18:51 -0400

    Use entityframework instead. Keep helper as reference only.

--- ModernUO-API/Helpers/DatabaseHelper.cs
+++ ModernUO-API/Helpers/DatabaseHelper.cs
@@ -1,84 +1,84 @@
-﻿using SQLite;
+﻿//using SQLite;
 
-namespace ModernUO_API.Helpers;
+//namespace ModernUO_API.Helpers;
 
-public static class DatabaseHelper
-{
-    private static readonly string _localAppPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-    private static readonly string _appPath = Path.Combine(_localAppPath, "ModernUO", "ServerAccounts");
+//public static class DatabaseHelper
+//{
+//    private static readonly string _localAppPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+//    private static readonly string _appPath = Path.Combine(_localAppPath, "ModernUO", "ServerAccounts");
 
-    private static readonly string _accounts = Path.Combine(_appPath, "Accounts.db");
+//    private static readonly string _accounts = Path.Combine(_appPath, "Accounts.db");
 
-    public static void InitializeFolder()
-    {
-        if (!Directory.Exists(_appPath)) Directory.CreateDirectory(_appPath);
-    }
+//    public static void InitializeFolder()
+//    {
+//        if (!Directory.Exists(_appPath)) Directory.CreateDirectory(_appPath);
+//    }
 
-    #region Database CRUD
+//    #region Database CRUD
 
-    #region Create
+//    #region Create
 
-    public static void Insert<T>(T item)
-    {
-        using SQLiteConnection connection = new(_accounts);
-        connection.CreateTable<T>();
-        connection.Insert(item);
-    }
+//    public static void Insert<T>(T item)
+//    {
+//        using SQLiteConnection connection = new(_accounts);
+//        connection.CreateTable<T>();
+//        connection.Insert(item);
+//    }
 
-    public static void Insert<T>(List<T> items)
-    {
-        using SQLiteConnection connection = new(_accounts);
-        connection.CreateTable<T>();
-        connection.InsertAll(items);
-    }
+//    public static void Insert<T>(List<T> items)
+//    {
+//        using SQLiteConnection connection = new(_accounts);
+//        connection.CreateTable<T>();
+//        connection.InsertAll(items);
+//    }
 
-    #endregion Create
+//    #endregion Create
 
-    #region Read
+//    #region Read
 
-    public static List<T> Read<T>() where T : new()
-    {
-        SQLiteConnectionString connectionString = new(_accounts, openFlags: SQLiteOpenFlags.ReadOnly | SQLiteOpenFlags.SharedCache, storeDateTimeAsTicks: true);
-        using SQLiteConnection connection = new(connectionString);
-        return [.. connection.Table<T>()];
-    }
+//    public static List<T> Read<T>() where T : new()
+//    {
+//        SQLiteConnectionString connectionString = new(_accounts, openFlags: SQLiteOpenFlags.ReadOnly | SQLiteOpenFlags.SharedCache, storeDateTimeAsTicks: true);
+//        using SQLiteConnection connection = new(connectionString);
+//        return [.. connection.Table<T>()];
+//    }
 
-    #endregion Read
+//    #endregion Read
 
-    #region Update
+//    #region Update
 
-    public static void Update<T>(T item)
-    {
-        using SQLiteConnection connection = new(_accounts);
-        connection.CreateTable<T>();
-        connection.Update(item);
-    }
+//    public static void Update<T>(T item)
+//    {
+//        using SQLiteConnection connection = new(_accounts);
+//        connection.CreateTable<T>();
+//        connection.Update(item);
+//    }
 
-    public static void Update<T>(List<T> items)
-    {
-        using SQLiteConnection connection = new(_accounts);
-        connection.CreateTable<T>();
-        connection.UpdateAll(items);
-    }
+//    public static void Update<T>(List<T> items)
+//    {
+//        using SQLiteConnection connection = new(_accounts);
+//        connection.CreateTable<T>();
+//        connection.UpdateAll(items);
+//    }
 
-    #endregion Update
+//    #endregion Update
 
-    #region Delete
+//    #region Delete
 
-    public static void Delete<T>(T item)
-    {
-        using SQLiteConnection connection = new(_accounts);
-        connection.CreateTable<T>();
-        connection.Delete(item);
-    }
+//    public static void Delete<T>(T item)
+//    {
+//        using SQLiteConnection connection = new(_accounts);
+//        connection.CreateTable<T>();
+//        connection.Delete(item);
+//    }
 
-    public static void DropTable<T>() where T : new()
-    {
-        using SQLiteConnection connection = new(_accounts);
-        connection.DropTable<T>();
-    }
+//    public static void DropTable<T>() where T : new()
+//    {
+//        using SQLiteConnection connection = new(_accounts);
+//        connection.DropTable<T>();
+//    }
 
-    #endregion Delete
+//    #endregion Delete
 
-    #endregion Database CRUD
-}
+//    #endregion Database CRUD
+//}
