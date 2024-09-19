f08f4ffda8a8c8d290c8e4a4e17b0350abeca7cd
Derek Gooding <djgooding@live.com>
2024-09-19 13:56:55 -0400

    Some cleanup + upgrade swagger

--- ModernUO-Gateway/Model/GameServerListing.cs
+++ ModernUO-Gateway/Model/GameServerListing.cs
@@ -1,12 +1,12 @@
 using System.Net;
 
-namespace ModernUO_Gateway.Model;
+namespace Model;
 
 public record GameServerListing
 {
     public Guid Id { get; } = Guid.NewGuid();
     public string Name { get; init; } = "Unknown Game Server";
-    public IPEndPoint[] ListeningAddresses { get; init; } = Array.Empty<IPEndPoint>();
-    public bool IsOnline { get; init; } = false;
+    public IPEndPoint[] ListeningAddresses { get; init; } = [];
+    public bool IsOnline { get; init; }
     public DateTime LastRefresh { get; set; } = DateTime.MaxValue;
 }
--- ModernUO-Gateway/Model/UOGatewayServer.cs
+++ ModernUO-Gateway/Model/UOGatewayServer.cs
@@ -1,36 +1,32 @@
 using System.Net;
 using System.Net.Sockets;
 
-namespace ModernUO_Gateway.Model;
+namespace Model;
 
-public class UOGatewayServer : IHostedService
+public class UOGatewayServer(IHostApplicationLifetime hostApplicationLifetime) : IHostedService
 {
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+    private readonly IHostApplicationLifetime _hostApplicationLifetime = hostApplicationLifetime;
     private readonly HashSet<TcpClient> _tcpClients = [];
-
-    public UOGatewayServer(IHostApplicationLifetime hostApplicationLifetime) => _hostApplicationLifetime = hostApplicationLifetime;
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _ = Task.Run(RunServer, cancellationToken);
         return Task.CompletedTask;
     }
-    
+
     private async Task RunServer()
     {
         CancellationToken stoppingToken = _hostApplicationLifetime.ApplicationStopping;
-        TcpListener tcpListener = new TcpListener(IPAddress.Any, 2593);
+        TcpListener tcpListener = new(IPAddress.Any, 2593);
         tcpListener.Start();
-        
+
         while (!stoppingToken.IsCancellationRequested)
         {
             TcpClient client = await tcpListener.AcceptTcpClientAsync(stoppingToken);
             client.NoDelay = true;
             if (stoppingToken.IsCancellationRequested)
-            {
                 break;
-            }
-            
+
             _tcpClients.Add(client);
             _ = Task.Run(() => HandleClientAsync(client, stoppingToken), stoppingToken);
         }
@@ -52,10 +48,8 @@ public class UOGatewayServer : IHostedService
     private async Task HandleClientAsync(TcpClient client, CancellationToken cancellationToken)
     {
         if (cancellationToken.IsCancellationRequested)
-        {
             return;
-        }
-        
+
         // Implement your logic to handle the TCP client
         // You can use _myService here as needed
     }
